f69171a9160f652ea589a72305db41c3982df608
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-26 00:24:17 +0530

    Initial Commit

---
+++ Shared/CoViDtrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoViDtrackerApp.swift
+//  Shared
+//
+//  Created by Japneet Singh on /266/20.
+//
+
+import SwiftUI
+
+@main
+struct CoViDtrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
