676d0b464e6c02c080a296ad23dd90088c509d31
Japneet Singh <singhjapneetssa@gmail.com>
2020-06-26 02:04:06 +0530

    First commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries: [Country] = []
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView{
+            List{
+                ForEach(countries){
+                    country in
+                    CountryCell(country: country)
+                   
+                }
+            }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(countries: testData)
+            .preferredColorScheme(.dark)
+    }
+}
+
+struct CountryCell: View {
+    var country : Country
+    var body: some View {
+        NavigationLink(destination: CountryDetail(country: country))
+        {
+            Text(country.thumbnail).font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    Text(country.pos)
+                        .font(.subheadline)
+                        .fontWeight(.thin).foregroundColor(.secondary)
+                    Text(country.name).font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/).fontWeight(.semibold)
+                }
+                
+                HStack(alignment: .bottom) {
+                    
+                    Text("🟡 \(country.confirmedCount)").font(.footnote)
+                    country.recoveredCount == 0 ? Text("🟢 N/A").font(.footnote) : Text("🟢 \(country.recoveredCount)").font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
+                    Text("🔴 \(country.deceasedCount)").font(.footnote)
+                    
+                    
+                }
+                .padding(.vertical, 5.0)
+                
+                
+            }
+        }.navigationTitle("Worldwide")
     }
 }
--- Shared/CountryDetail.swift
+++ Shared/CountryDetail.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct CountryDetail: View {
+    var country: Country
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Confirmed Cases").font(.largeTitle).fontWeight(.bold)
+                        Text("\(country.confirmedCount)").font(.title)
+                    }
+                    Spacer()
+                }
+                .padding(.vertical).background(Color.yellow)
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Recovered").font(.largeTitle).fontWeight(.bold)
+                        country.recoveredCount == 0 ? Text(" N/A").font(.title) : Text(" \(country.recoveredCount)").font(.title)
+                    }
+                    Spacer()
+                }.padding(.vertical).background(Color.green)
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Deceased").font(.largeTitle).fontWeight(.bold)
+                        Text("\(country.deceasedCount)").font(.title)
+                    }
+                    Spacer()
+                }.padding(.vertical).background(Color.red)
+            }
+            .navigationTitle(country.thumbnail + " " + country.name).navigationBarTitleDisplayMode(.inline)
+            
+        
+        
     }
 }
 
 struct CountryDetail_Previews: PreviewProvider {
     static var previews: some View {
-        CountryDetail()
+        NavigationView{
+        CountryDetail(country: testData[0])
+        }
     }
 }
